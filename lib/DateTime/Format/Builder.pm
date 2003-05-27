de9902ffa90a6ce8e22139d91fa2d0b306a27a4b
Iain Truskett (PAUSE Custodial Account) <modules@perl.org>
2003-05-27 23:36:00 +0000

    Import of SPOON/DateTime-Format-Builder-0.72 from CPAN.
    gitpan-cpan-distribution: DateTime-Format-Builder
    gitpan-cpan-version:      0.72
    gitpan-cpan-path:         SPOON/DateTime-Format-Builder-0.72.tar.gz
    gitpan-cpan-author:       SPOON
    gitpan-cpan-maturity:     released

--- lib/DateTime/Format/Builder.pm
+++ lib/DateTime/Format/Builder.pm
@@ -1,5 +1,5 @@
 package DateTime::Format::Builder;
-# $Id: Builder.pm,v 1.20 2003/05/23 07:46:21 koschei Exp $
+# $Id: Builder.pm,v 1.21 2003/05/27 23:27:27 koschei Exp $
 
 =begin comments
 
@@ -17,7 +17,7 @@ use Params::Validate qw(
 use vars qw( $VERSION );
 
 my $parser = 'DateTime::Format::Builder::Parser';
-$VERSION = '0.71';
+$VERSION = '0.72';
 
 # Developer oriented methods
 
